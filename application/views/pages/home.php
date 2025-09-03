0860a9b9f3a14480cdd8007c4c78b237553b89b8
xynoan <ncmorva@gmail.com>
2025-09-03 12:23:28 +0800

    feat: add feedback when there's no manuals added yet

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -15,7 +15,9 @@
         <input type="text" class="form-control" id="search" placeholder="Search...">
     </label>
 </div>
-<div class="table-responsive">
+<?php if (!empty($trainings)): ?>
+<!-- <div class="table-responsive"> -->
+<div>
     <table class="table table-bordered">
         <thead>
             <tr>
@@ -50,7 +52,7 @@
                             </button>
                             <ul class="dropdown-menu">
                                 <li>
-                                    <a class="d-flex align-items-center gap-2 dropdown-item text-primary" href="/edit">
+                                    <a class="d-flex align-items-center gap-2 dropdown-item text-primary" href="/edit?id=<?= $training['id'] ?>">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
@@ -74,6 +76,10 @@
         </tbody>
     </table>
 </div>
+<?php endif; ?>
+<?php if (empty($trainings)): ?>
+    <p class="text-center fs-4">No training manuals found.</p>
+<?php endif; ?>
 <div>
     <?= $pagination ?>
 </div>
