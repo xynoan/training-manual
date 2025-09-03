1af85f096376e413e6deb8371cba5d7e34e9a5db
xynoan <ncmorva@gmail.com>
2025-09-03 13:41:25 +0800

    add: update_training method in training model

--- application/models/Training_model.php
+++ application/models/Training_model.php
@@ -37,6 +37,39 @@ class Training_model extends CI_Model
         return $manual_id;
     }
 
+    public function update_training($id, $data)
+    {
+        $training_data = array(
+            'title' => $data['title']
+        );
+
+        $this->db->where('id', $id);
+        $this->db->update('tbl_training_manual', $training_data);
+
+        if (isset($data['name']) && !empty($data['name'])) {
+            $file_name = is_array($data['name']) ? implode(', ', $data['name']) : $data['name'];
+
+            $files = array(
+                'file_name' => $file_name,
+                'file_path' => 'hard-coded for now'
+            );
+
+            $this->db->where('manual_id', $id);
+            $this->db->update('tbl_training_manual_file', $files);
+        }
+
+        if (isset($data['note'])) {
+            $notes_data = array(
+                'note' => $data['note']
+            );
+
+            $this->db->where('manual_id', $id);
+            $this->db->update('tbl_training_manual_notes', $notes_data);
+        }
+
+        return true;
+    }
+
     public function delete_training($id)
     {
         return $this->db->delete('tbl_training_manual', ['id' => $id]);
