5226d0ad1ace0ee970de4b51dd5167c6332ae5f7
xynoan <ncmorva@gmail.com>
2025-09-08 12:02:01 +0800

    refactor: use base url for links

--- application/views/pages/add.php
+++ application/views/pages/add.php
@@ -1,5 +1,5 @@
 <?php require 'partials/floating-alert.php' ?>
-<form action="/add" method="post" enctype="multipart/form-data">
+<form action="<?= base_url('add') ?>" method="post" enctype="multipart/form-data">
     <div class="d-flex gap-2">
         <button type="submit" class="btn btn-primary d-flex align-items-center gap-1" onclick="showFloatingAlert()">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-floppy2" viewBox="0 0 16 16">
@@ -7,7 +7,7 @@
             </svg>
             Submit
         </button>
-        <a href="/" class="btn btn-danger d-flex align-items-center gap-1">
+        <a href="<?= base_url() ?>" class="btn btn-danger d-flex align-items-center gap-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-skip-backward-fill" viewBox="0 0 16 16">
                 <path d="M.5 3.5A.5.5 0 0 0 0 4v8a.5.5 0 0 0 1 0V8.753l6.267 3.636c.54.313 1.233-.066 1.233-.697v-2.94l6.267 3.636c.54.314 1.233-.065 1.233-.696V4.308c0-.63-.693-1.01-1.233-.696L8.5 7.248v-2.94c0-.63-.692-1.01-1.233-.696L1 7.248V4a.5.5 0 0 0-.5-.5" />
             </svg>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,4 +1,4 @@
-<a href="/add" class="btn btn-danger d-flex align-items-center gap-2">
+    <a href="<?= base_url('add') ?>" class="btn btn-danger d-flex align-items-center gap-2">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
         class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
         <path
@@ -100,7 +100,7 @@
                         <td>
                             <?php foreach ($training['file_names'] as $index => $file_name): ?>
                                 <a class="link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover file-preview-link"
-                                    href="/pages/preview_file/<?= $training['id'] ?>/<?= $index ?>"
+                                    href="<?= base_url('pages/preview_file/' . $training['id'] . '/' . $index) ?>"
                                     data-training-id="<?= $training['id'] ?>"
                                     data-file-index="<?= $index ?>"
                                     data-file-name="<?= htmlspecialchars($file_name) ?>"
@@ -120,7 +120,7 @@
                                 </button>
                                 <ul class="dropdown-menu">
                                     <li>
-                                        <a class="d-flex align-items-center gap-2 dropdown-item text-primary" href="/edit?id=<?= $training['id'] ?>">
+                                        <a class="d-flex align-items-center gap-2 dropdown-item text-primary" href="<?= base_url('edit?id=' . $training['id']) ?>">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
@@ -130,7 +130,7 @@
                                     </li>
                                     <li>
                                         <a class="d-flex align-items-center gap-2 dropdown-item text-danger"
-                                            href="/pages/delete/<?= $training['id'] ?>"
+                                            href="<?= base_url('pages/delete/' . $training['id']) ?>"
                                             onclick="return confirm('Are you sure you want to delete this training manual?');">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                                 <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
